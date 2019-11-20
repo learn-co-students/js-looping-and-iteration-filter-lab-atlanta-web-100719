d907958a46170bcf4373e0f7dcb9f75eb3f18852
clarkj99 <clark@stontech.net>
2019-11-20 17:13:03 -0500

    Done.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function fuzzyMatch(drivers, snip) {
         return driver.slice(0, snip.length) == snip;
     })
 }
+
 function matchName(drivers, name) {
     return drivers.filter(function (driver) {
         return driver.name.toLowerCase() == name.toLowerCase();
